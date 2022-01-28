3468812a39397ee6f785db1c21c6cca4f1792a2c
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-28 15:31:18 -0800

    Initial commit. Created the package structure

---
+++ src/main/java/com/motelreg/motel_registration/MotelRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.motelreg.motel_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotelRegistrationApplication.class, args);
+    }
+
+}
