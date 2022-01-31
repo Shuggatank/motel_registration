28c5a4ab793ce7429290e70aec550a229c2ac8e8
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-31 07:16:08 -0800

    Added the JWT methods in LoginResponse

--- src/main/java/com/motelreg/motel_registration/model/Response/LoginResponse.java
+++ src/main/java/com/motelreg/motel_registration/model/Response/LoginResponse.java
@@ -1,4 +1,17 @@
 package com.motelreg.motel_registration.model.Response;
 
 public class LoginResponse {
+    private String JWT;
+
+    public LoginResponse(String JWT) {
+        this.JWT = JWT;
+    }
+
+    public String getJWT() {
+        return JWT;
+    }
+
+    public void setJWT(String JWT) {
+        this.JWT = JWT;
+    }
 }
