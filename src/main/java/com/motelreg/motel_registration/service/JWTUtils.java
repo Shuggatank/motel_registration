73965db98e6113450be94eaca87f0b56223c3386
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-31 07:25:02 -0800

    JWTUtils - added method to create a token that contains username, issued time, and expiration time. Expiration is set to 10 hours.

--- src/main/java/com/motelreg/motel_registration/service/JWTUtils.java
+++ src/main/java/com/motelreg/motel_registration/service/JWTUtils.java
@@ -1,8 +1,11 @@
 package com.motelreg.motel_registration.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,8 +13,16 @@ import java.util.Map;
 public class JWTUtils {
     String SECRET_KEY = "TestKey";
 
+    //Token Generation
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails.getUsername());
+    }
+
+    //Token Creation and token expiry is set to 10 hours
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 }
