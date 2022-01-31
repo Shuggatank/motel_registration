e7dc48d1f6e47817103901be47713801a639e2c4
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-31 07:44:09 -0800

    JwtRequestFilter - Added dependency injection for MyUserDetailsService and JWTUtils

--- src/main/java/com/motelreg/motel_registration/service/JwtRequestFilter.java
+++ src/main/java/com/motelreg/motel_registration/service/JwtRequestFilter.java
@@ -1,4 +1,18 @@
 package com.motelreg.motel_registration.service;
 
-public class JwtRequestFilter {
+import com.motelreg.motel_registration.security.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+
 }
