534ade57e1821782c2717934c7738fc0f6c61223
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-31 07:22:44 -0800

    JWTUtils - added secret key and method to generate tokens

--- src/main/java/com/motelreg/motel_registration/service/JWTUtils.java
+++ src/main/java/com/motelreg/motel_registration/service/JWTUtils.java
@@ -1,4 +1,17 @@
 package com.motelreg.motel_registration.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class JWTUtils {
+    String SECRET_KEY = "TestKey";
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
 }
