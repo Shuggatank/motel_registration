276c78bdf2fe601424c41b5d1786859d5bc0e3f9
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-30 20:28:25 -0800

    ManagerController has been filled out, may need fixing later.

--- src/main/java/com/motelreg/motel_registration/controller/ManagerController.java
+++ src/main/java/com/motelreg/motel_registration/controller/ManagerController.java
@@ -3,9 +3,10 @@ package com.motelreg.motel_registration.controller;
 import com.motelreg.motel_registration.model.Manager;
 import com.motelreg.motel_registration.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,17 +16,27 @@ public class ManagerController {
 
     private ManagerService managerService;
 
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Autowired
     public void setManagerService(ManagerService managerService) {
         this.managerService = managerService;
     }
 
-    @GetMapping("/managers")
-    public List<Manager> getManagers() {
-        System.out.println("getting all managers");
-        return managerService.getManagers();
+    @PostMapping("/register")
+    public Manager createManager(@RequestBody Manager managerObject) {
+        System.out.println("calling createManager ==>");
+        return managerService.createManager(managerObject);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginManager(@RequestBody LoginRequest loginRequest) {
+        System.out.println("calling loginManager ==>");
+        return managerService.loginManager(loginRequest);
+    }
+
 }
