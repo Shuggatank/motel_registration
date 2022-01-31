09aa954f5e5965037d99c491a31ce0b11ed5dd15
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-30 16:31:46 -0800

    Created the method and endpoint for getting all registrations

--- src/main/java/com/motelreg/motel_registration/controller/RegistrationController.java
+++ src/main/java/com/motelreg/motel_registration/controller/RegistrationController.java
@@ -1,6 +1,30 @@
 package com.motelreg.motel_registration.controller;
 
+import com.motelreg.motel_registration.model.Customer;
+import com.motelreg.motel_registration.model.Registration;
+import com.motelreg.motel_registration.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api")
 public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @Autowired
+    public void setRegistrationService(RegistrationService registrationService){
+        this.registrationService = registrationService;
+    }
+
+    @GetMapping("/registrations")
+    public List<Registration> getAllRegistrations() {
+        System.out.println("calling getAllRegistrations");
+        return registrationService.getAllRegistrations();
+    }
 
 }
--- src/main/java/com/motelreg/motel_registration/service/RegistrationService.java
+++ src/main/java/com/motelreg/motel_registration/service/RegistrationService.java
@@ -1,4 +1,23 @@
 package com.motelreg.motel_registration.service;
 
+import com.motelreg.motel_registration.model.Registration;
+import com.motelreg.motel_registration.repository.RegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RegistrationService {
+    private RegistrationRepository registrationRepository;
+
+    @Autowired
+    public void setRegistrationRepository(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+
+    public List<Registration> getAllRegistrations() {
+        System.out.println("Calling getAllRegistrations");
+        return registrationRepository.findAll();
+    }
 }
