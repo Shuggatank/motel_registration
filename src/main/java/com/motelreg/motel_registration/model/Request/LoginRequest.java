0d7a1060f7af190a738672348199c51c9a8f9798
Dipesh Patel <dipeshgpatel@gmail.com>
2022-01-30 20:31:16 -0800

    Created LoginRequest

---
+++ src/main/java/com/motelreg/motel_registration/model/Request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.motelreg.motel_registration.model.Request;
+
+public class LoginRequest {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
